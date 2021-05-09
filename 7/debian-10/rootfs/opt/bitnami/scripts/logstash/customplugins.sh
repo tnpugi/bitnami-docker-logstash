7712655b00753e57914363dcabb7f070340706e7
jonathan.lonceras <jonathan.lonceras@hpe.com>
2021-05-09 16:02:36 +0800

    added extra standard sets/checks

--- 7/debian-10/rootfs/opt/bitnami/scripts/logstash/customplugins.sh
+++ 7/debian-10/rootfs/opt/bitnami/scripts/logstash/customplugins.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# shellcheck disable=SC1091
+
+set -o errexit
+set -o nounset
+set -o pipefail
+# set -o xtrace # Uncomment this line for debugging purpose
+
+# Load libraries
 . /opt/bitnami/logstash/bin/logstash-plugin install logstash-output-newrelic
 
+# Load Logstash environment variables
+eval "$(logstash_env)"
