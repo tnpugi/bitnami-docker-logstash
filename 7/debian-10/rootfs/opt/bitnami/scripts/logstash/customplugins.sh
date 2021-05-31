7e15de7d1f3b1cfcf1231fe255a10f7bd184cef5
jonathan.lonceras <jonathan.lonceras@hpe.com>
2021-05-31 15:54:08 +0800

    added customplugins script

--- 7/debian-10/rootfs/opt/bitnami/scripts/logstash/customplugins.sh
+++ 7/debian-10/rootfs/opt/bitnami/scripts/logstash/customplugins.sh
@@ -8,7 +8,21 @@ set -o pipefail
 # set -o xtrace # Uncomment this line for debugging purpose
 
 # Load libraries
-. /opt/bitnami/logstash/bin/logstash-plugin install logstash-output-newrelic
+. /opt/bitnami/scripts/liblog.sh
+. /opt/bitnami/scripts/liblogstash.sh
 
 # Load Logstash environment variables
 eval "$(logstash_env)"
+
+info "Temporarily setting up heap size to 512m ..."
+LOGSTASH_HEAP_SIZE_ORIG=$LOGSTASH_HEAP_SIZE
+LOGSTASH_HEAP_SIZE=512m
+logstash_set_heap_size
+
+info "Installing additional logstash plugins..."
+/opt/bitnami/logstash/bin/logstash-plugin install logstash-output-newrelic
+/opt/bitnami/logstash/bin/logstash-plugin install logstash-output-jdbc
+
+
+info "Setting LOGSTASH_HEAP_SIZE to configured default ..."
+LOGSTASH_HEAP_SIZE=$LOGSTASH_HEAP_SIZE_ORIG
